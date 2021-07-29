257ca8604b1be26ff7c1f728f935f8af9e065263
JefferssonSemin <jeffesemin@hotmail.com>
2021-07-29 18:40:11 -0300

    chore: add mid and create user

--- src/index.js
+++ src/index.js
@@ -1,25 +1,50 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4, v4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const {username} = request.headers
+
+  const user = users.find((user) => user.username === username)
+
+  if(!user){
+    return response.status(400).json({error: "User not found."})
+  }
+
+  request.username = username
+
+  return next()
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+    const {name, username} = request.body
+
+    const user ={
+      id : v4(),
+      name,
+      username,
+      todo: []
+    }
+
+    users.push(user)
+
+    return response.status(201).json(user)
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+   const {username} = request
+
+   const {todo} = users.find((user) => user.username === username)
+
+   return response.json(todo)
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
